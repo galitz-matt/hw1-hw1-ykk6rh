9f872476474c02a6cc02abcedf60433d60c01f07
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-08-31 14:35:19 -0400

    Modified readCSV() function, added main method for testing

--- src/main/java/edu/virginia/sde/hw1/CSVReader.java
+++ src/main/java/edu/virginia/sde/hw1/CSVReader.java
@@ -1,21 +1,41 @@
 package main.java.edu.virginia.sde.hw1;
+
 import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
 
 public class CSVReader {
 
-    public static List<String> readCSV(String filePath) throws FileNotFoundException {
+    public static List<String> readCSV(String filePath) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             List<String> allLines = new ArrayList<>();
-            String line = reader.readLine(); // skip first line (State, Population)
+            reader.readLine(); // skip first line
+            String line;
             while((line = reader.readLine()) != null) {
                 allLines.add(line);
             }
             return allLines;
         }
+        catch (FileNotFoundException e) {
+            System.out.println("Error - .csv file does not exist or was entered incorrectly");
+            System.exit(1);
+        }
         catch (IOException e) {
-            throw new FileNotFoundException("File not found - file does not exist or was entered incorrectly");
+            System.out.println("Error - make sure correct file path was entered");
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String filePath = args[0];
+            String[] lines = readCSV(filePath).toArray(new String[0]);
+            System.out.println(Arrays.toString(lines));
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error - enter f");
+            System.exit(1);
         }
     }
+
 }
