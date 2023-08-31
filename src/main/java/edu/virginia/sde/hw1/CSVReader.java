ef999a4bf1c0242659c6bf5d0b2c3e212028f959
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-08-31 13:59:48 -0400

    Created readCSV method, stores each line without parsing

--- src/main/java/edu/virginia/sde/hw1/CSVReader.java
+++ src/main/java/edu/virginia/sde/hw1/CSVReader.java
@@ -5,5 +5,17 @@ import java.util.*;
 
 public class CSVReader {
 
-
+    public static List<String> readCSV(String filePath) throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<String> allLines = new ArrayList<>();
+            String line = reader.readLine(); // skip first line (State, Population)
+            while((line = reader.readLine()) != null) {
+                allLines.add(line);
+            }
+            return allLines;
+        }
+        catch (IOException e) {
+            throw new FileNotFoundException("File not found - file does not exist or was entered incorrectly");
+        }
+    }
 }
