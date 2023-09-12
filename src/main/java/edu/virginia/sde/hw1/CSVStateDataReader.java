add37da33638e526249de33cd4b3eeb56a760e89
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-12 14:41:15 -0400

    Moved getStatesInAlphabeticalOrder to main class

--- src/main/java/edu/virginia/sde/hw1/CSVStateDataReader.java
+++ src/main/java/edu/virginia/sde/hw1/CSVStateDataReader.java
@@ -3,9 +3,9 @@ package main.java.edu.virginia.sde.hw1;
 import java.io.*;
 import java.util.*;
 
-public class CSVReader implements Reader{
+public class CSVStateDataReader implements StateDataReader {
 
-    public static List<String> getStateDataList(String filePath) {
+    public static List<String> getStateDataList(String filePath) throws IOException {
         try (var reader = new BufferedReader(new FileReader(filePath))) {
             var lineList = new ArrayList<String>();
             reader.readLine(); // skip header row
@@ -15,45 +15,37 @@ public class CSVReader implements Reader{
             }
             return lineList;
         } catch (FileNotFoundException e) {
-            System.out.printf("Error - file \"%s\" not found\n", filePath);
-            System.exit(1);
+            var errorMessage = String.format("Error - file \"%s\" not found\n", filePath);
+            throw new FileNotFoundException(errorMessage);
         } catch (IOException e) {
-            System.out.println("Error - verify input correctness");
-            System.exit(1);
+            throw new IOException("Error - verify input correctness");
         }
-        return null;
     }
 
-    public static List<String> getStatesInAlphabeticalOrder(Map<String, Integer> csvMap) {
-        List<String> orderedStates = new ArrayList<>(csvMap.keySet());
-        orderedStates.sort(CharSequence::compare);
-        return orderedStates;
-    }
-
-    public static Map<String, Integer> getStateDataMap(String filePath) throws IOException{
-        var csvList = getStateDataList(filePath);
-        var csvMap = new HashMap<String, Integer>();
-        for (int i = 0; i < csvList.size(); i++) {
-            var keyValue = csvList.get(i).split(",");
+    public static Map<String, Integer> getStateDataMap(String filePath) throws IOException {
+        var stateDataList = getStateDataList(filePath);
+        var stateDataMap = new HashMap<String, Integer>();
+        for (int i = 0; i < stateDataList.size(); i++) {
+            var keyValue = stateDataList.get(i).split(",");
             try {
                 var stateName = keyValue[0].strip();
                 var statePopulation = Integer.parseInt(keyValue[1].strip());
                 if (statePopulation >= 0) {
-                    csvMap.put(stateName, statePopulation);
+                    stateDataMap.put(stateName, statePopulation);
                 } else {
-                    System.out.printf("Line %d ignored - Population must be positive integer value - \"%s\" \n", i + 2, csvList.get(i));
+                    System.out.printf("Line %d ignored - Population must be positive integer value - \"%s\" \n", i + 2, stateDataList.get(i));
                 }
             } catch (NumberFormatException e) {
-                System.out.printf("Line %d ignored - Bad format - \"%s\"\n", i + 2, csvList.get(i));
+                System.out.printf("Line %d ignored - Bad format - \"%s\"\n", i + 2, stateDataList.get(i));
             } catch (ArrayIndexOutOfBoundsException e) {
-                String errorMessage = String.format("Error - Line %d has bad format - \"%s\"", i + 2, csvList.get(i));
+                String errorMessage = String.format("Error - Line %d has bad format - \"%s\"", i + 2, stateDataList.get(i));
                 throw new IOException(errorMessage);
             }
         }
-        if (csvMap.isEmpty()) {
+        if (stateDataMap.isEmpty()) {
             throw new IOException("\nError - .csv file is empty or no lines are properly formatted");
         }
-        return csvMap;
+        return stateDataMap;
     }
 
 }
--- src/main/java/edu/virginia/sde/hw1/Main.java
+++ src/main/java/edu/virginia/sde/hw1/Main.java
@@ -1,6 +1,9 @@
 package main.java.edu.virginia.sde.hw1;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -8,14 +11,20 @@ public class Main {
         if (args.length < 1) {
             throw new IOException("Error - No .csv file given");
         }
-        var stateData = new StateData(CSVReader.getMapFromCSV(args[0]));
+        var stateData = new StateData(CSVStateDataReader.getStateDataMap(args[0]));
         var totalSeats = args.length < 2 ? 435 : Integer.parseInt(args[1]);
         var divisor = getDivisor(stateData, totalSeats);
         jeffersonAlgorithm(stateData, totalSeats, divisor);
-        for (String stateName : CSVReader.getStatesInAlphabeticalOrder(stateData.statePopulationMap)) {
+        for (String stateName : getStatesInAlphabeticalOrder(stateData.statePopulationMap)) {
             var numStateSeats = stateData.apportionmentMap.get(stateName);
             System.out.printf("%s - %d\n", stateName, numStateSeats);
         }
+    }
+
+    private static List<String> getStatesInAlphabeticalOrder(Map<String, Integer> stateDataMap) {
+        List<String> orderedStates = new ArrayList<>(stateDataMap.keySet());
+        orderedStates.sort(CharSequence::compare);
+        return orderedStates;
     }
 
     private static double getDivisor(StateData stateData, int numSeats) {
