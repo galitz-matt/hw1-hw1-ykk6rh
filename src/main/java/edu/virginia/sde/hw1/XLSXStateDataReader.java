5c8228f3add10e078bb64df13af34e33f8dde0cc
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-12 18:47:10 -0400

    Created XLSXStateDataReader

---
+++ src/main/java/edu/virginia/sde/hw1/XLSXStateDataReader.java
@@ -0,0 +1,14 @@
+package main.java.edu.virginia.sde.hw1;
+
+import java.util.*;
+
+public class XLSXStateDataReader implements StateDataReader {
+
+    public List<String> getLines() {
+        return null;
+    }
+
+    public Map<String, Integer> getStateDataMap() {
+        return null;
+    }
+}
