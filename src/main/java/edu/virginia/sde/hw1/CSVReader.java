3485e28a74244e814fbd784881413a63a91c3756
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-14 17:07:19 -0400

    fix: amend variable name discrepancy in setStatePopulations

--- src/main/java/edu/virginia/sde/hw1/CSVReader.java
+++ src/main/java/edu/virginia/sde/hw1/CSVReader.java
@@ -7,52 +7,53 @@ public class CSVReader implements Reader {
 
     private final List<String> fileLines;
     private final Map<String, Integer> statePopulations;
-    private int STATE,
-            POPULATION;
+//    private int STATE,
+//            POPULATION;
 
     public CSVReader(String filePath) {
         fileLines = setFileLines(filePath);
         statePopulations = setStatePopulations(fileLines);
     }
 
-    private static String[] getParsedHeadings(String headings) {
-        return Arrays.stream(headings.split(",")).map(String::trim).map(String::toLowerCase).toArray(String[]::new);
-    }
+//    private static String[] getParsedHeadings(String headings) {
+//        return Arrays.stream(headings.split(",")).map(String::trim).map(String::toLowerCase).toArray(String[]::new);
+//    }
 
-    private int findStateHeading(String[] parsedHeadings) {
-        for (int i = 0; i < parsedHeadings.length; i++) {
-            if (parsedHeadings[i].equalsIgnoreCase("state")) {
-                return i;
-            }
-        }
-        throw new RuntimeException("\"State\" column heading not found");
-    }
-
-    private int findPopulationHeading(String[] parsedHeader) {
-        for (int i = 0; i < parsedHeader.length; i++) {
-            if (parsedHeader[i].equalsIgnoreCase("population")) {
-                return i;
-            }
-        }
-        throw new RuntimeException("\"Population\" column heading not found");
-    }
-
-    private void processHeadings(BufferedReader reader) {
-        try {
-            var headings = reader.readLine();
-            var parsedHeadings = getParsedHeadings(headings);
-            STATE = findStateHeading(parsedHeadings);
-            POPULATION = findPopulationHeading(parsedHeadings);
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private int findStateHeading(String[] parsedHeadings) {
+//        for (int i = 0; i < parsedHeadings.length; i++) {
+//            System.out.println(Arrays.toString(parsedHeadings));
+//            if (parsedHeadings[i].equalsIgnoreCase("state")) {
+//                return i;
+//            }
+//        }
+//        throw new RuntimeException("\"State\" column heading not found");
+//    }
+//
+//    private int findPopulationHeading(String[] parsedHeader) {
+//        for (int i = 0; i < parsedHeader.length; i++) {
+//            if (parsedHeader[i].equalsIgnoreCase("population")) {
+//                return i;
+//            }
+//        }
+//        throw new RuntimeException("\"Population\" column heading not found");
+//    }
+//
+//    private void processHeadings(BufferedReader reader) {
+//        try {
+//            var headings = reader.readLine();
+//            var parsedHeadings = getParsedHeadings(headings);
+//            STATE = findStateHeading(parsedHeadings);
+//            POPULATION = findPopulationHeading(parsedHeadings);
+//        }
+//        catch (IOException e) {
+//            throw new RuntimeException("Error");
+//        }
+//    }
 
     private List<String> setFileLines(String filePath) {
         var fileLinesBuild = new ArrayList<String>();
         try (var reader = new BufferedReader(new FileReader(filePath))) {
-            processHeadings(reader); // skip header row
+            reader.readLine(); // skip header row
             String line;
             while ((line = reader.readLine()) != null) {
                 fileLinesBuild.add(line);
@@ -67,14 +68,14 @@ public class CSVReader implements Reader {
     }
 
     private Map<String, Integer> setStatePopulations(List<String> fileLines) {
-        var statePopulationBuild = new HashMap<String, Integer>();
+        var statePopulationsBuild = new HashMap<String, Integer>();
         for (int lineNumber = 0; lineNumber < fileLines.size(); lineNumber++) {
             var splitLine = fileLines.get(lineNumber).split(",");
             try {
-                var name = splitLine[STATE].strip();
-                var population = Integer.parseInt(splitLine[POPULATION].strip());
+                var name = splitLine[0].strip();
+                var population = Integer.parseInt(splitLine[1].strip());
                 if (population >= 0) {
-                    statePopulationBuild.put(name, population);
+                    statePopulationsBuild.put(name, population);
                 }
                 else {
                     System.out.printf("Line %d ignored - Population must be positive integer value - \"%s\" \n", lineNumber + 2, fileLines.get(lineNumber));
@@ -88,10 +89,10 @@ public class CSVReader implements Reader {
                 throw new RuntimeException(errorMessage);
             }
         }
-        if (statePopulations.isEmpty()) {
+        if (statePopulationsBuild.isEmpty()) {
             throw new RuntimeException("\nError - .csv file is empty or no lines are properly formatted");
         }
-        return statePopulationBuild;
+        return statePopulationsBuild;
     }
 
     public Map<String, Integer> getStatePopulations() {
