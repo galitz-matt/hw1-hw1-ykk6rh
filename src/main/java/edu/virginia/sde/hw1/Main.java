70fb6b3d536f1c48361ee61abc861d80a1241a53
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-14 21:04:02 -0400

    style: rename fileType to fileExtension in Main

--- src/main/java/edu/virginia/sde/hw1/Main.java
+++ src/main/java/edu/virginia/sde/hw1/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         MainTools.checkForFile(args);
         var filePath = args[0];
-        var fileType = MainTools.getFileExtension(filePath);
-        var csvReader = MainTools.getReader(filePath, fileType);
-        var stateData = new StateData(csvReader.getStatePopulations());
+        var fileExtension = MainTools.getFileExtension(filePath);
+        var reader = MainTools.getReader(filePath, fileExtension);
+        var stateData = new StateData(reader.getStatePopulations());
         var totalSeats = MainTools.numberOfSeats(args);
         var divisor = MainTools.getDivisor(stateData.getTotalPopulation(), totalSeats);
         MainTools.jeffersonAlgorithm(stateData, totalSeats, divisor);
