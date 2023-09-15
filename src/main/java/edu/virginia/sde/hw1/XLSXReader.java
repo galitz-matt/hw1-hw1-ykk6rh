bc5dcdebbf73544701114dd4178d1d7b3f2768c9
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-14 23:16:33 -0400

    feat: add setStatePopulations in XLSXReader, (will extract methods later)

--- src/main/java/edu/virginia/sde/hw1/XLSXReader.java
+++ src/main/java/edu/virginia/sde/hw1/XLSXReader.java
@@ -7,20 +7,44 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XLSXReader implements Reader {
 
-    private final List<String> fileLines = new ArrayList<>();
-    private final Map<String, Integer> statePopulation = new HashMap<>();
+    private Map<String, Integer> statePopulations;
     private static int STATE, POPULATION;
 
     public XLSXReader(String filePath) {
-        setFileLines(filePath);
-        setStatePopulation();
+
     }
 
-    private void setFileLines(String filePath) {
+    private Map<String, Integer> setStatePopulations(String excelFilePath) {
+        var statePopulationsBuild = new HashMap<String, Integer>();
+        try (FileInputStream inputStream = new FileInputStream(excelFilePath)) {
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+            Row headings = sheet.getRow(0);
+            for (Cell cell : headings) {
+                String heading = cell.getStringCellValue().trim();
+                if (heading.equalsIgnoreCase("state")) {
+                    STATE = cell.getColumnIndex();
+                }
+                else if (heading.equalsIgnoreCase("population")) {
+                    POPULATION = cell.getColumnIndex();
+                }
+            }
+            if (STATE == -1 || POPULATION == -1) {
+                throw new RuntimeException(ErrorMessages.columnHeadingNotFoundError(STATE == -1 ? "State" : "Population"));
+            }
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+                String state = row.getCell(STATE).getStringCellValue().trim();
+                int population = (int) row.getCell(POPULATION).getNumericCellValue();
+                statePopulationsBuild.put(state, population);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessages.inputError());
+        }
+        return statePopulationsBuild;
     }
 
-    private void setStatePopulation() {
+    public Map<String, Integer> getStatePopulations() {
+        return statePopulations;
     }
-
-    public Map<String, Integer> getStatePopulations() { return null; }
 }
